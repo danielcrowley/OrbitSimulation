e57c1c32200d713f860a080f67be2f98b31d15b1
Daniel Crowley <dan@relectrify.com>
2024-07-28 16:33:54 +1000

    Comment out atomsopheric colouring

--- src/earth.js
+++ src/earth.js
@@ -158,9 +158,9 @@ export class Earth {
     
             // adding small amount of atmospheric coloring to make it more realistic
             // fine tune the first constant for stronger or weaker effect
-            float intensity = 1.4 - dot( geometryNormal, vec3( 0.0, 0.0, 1.0 ) );
-            vec3 atmosphere = vec3( 0.3, 0.6, 1.0 ) * pow(intensity, 5.0);
-            diffuseColor.rgb += atmosphere;
+            // float intensity = 1.4 - dot( geometryNormal, vec3( 0.0, 0.0, 1.0 ) );
+            // vec3 atmosphere = vec3( 0.3, 0.6, 1.0 ) * pow(intensity, 5.0);
+            // diffuseColor.rgb += atmosphere;
             `)
     
             // need save to userData.shader in order to enable our code to update values in the shader uniforms,
