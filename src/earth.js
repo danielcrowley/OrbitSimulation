8526ea906fc86652352b49cf398ff2ac3f721005
Daniel Crowley <dan@relectrify.com>
2024-07-28 16:10:06 +1000

    Debug WebGL, minimise section with error

--- src/earth.js
+++ src/earth.js
@@ -89,84 +89,84 @@ export class Earth {
         // we need uv_xOffset so as to act as a means to calibrate the offset of the clouds shadows on earth(especially when earth and cloud rotate at different speeds)
         // the way I need to use fracts here is to get a correct calculated result of the cloud texture offset as it moves,
         // arrived at current method by doing the enumeration of cases (writing them down truly helps, don't keep everything in your head!)
-        // earthMat.onBeforeCompile = function( shader ) {
-        //     shader.uniforms.tClouds = { value: cloudsMap }
-        //     shader.uniforms.tClouds.value.wrapS = THREE.RepeatWrapping;
-        //     shader.uniforms.uv_xOffset = { value: 0 }
-        //     shader.fragmentShader = shader.fragmentShader.replace('#include <common>', `
-        //     #include <common>
-        //     uniform sampler2D tClouds;
-        //     uniform float uv_xOffset;
-        //     `);
-        //     shader.fragmentShader = shader.fragmentShader.replace('#include <roughnessmap_fragment>', `
-        //     float roughnessFactor = roughness;
+        earthMat.onBeforeCompile = function( shader ) {
+            shader.uniforms.tClouds = { value: cloudsMap }
+            shader.uniforms.tClouds.value.wrapS = THREE.RepeatWrapping;
+            shader.uniforms.uv_xOffset = { value: 0 }
+            shader.fragmentShader = shader.fragmentShader.replace('#include <common>', `
+            #include <common>
+            uniform sampler2D tClouds;
+            uniform float uv_xOffset;
+            `);
+            shader.fragmentShader = shader.fragmentShader.replace('#include <roughnessmap_fragment>', `
+            float roughnessFactor = roughness;
     
-        //     #ifdef USE_ROUGHNESSMAP
+            #ifdef USE_ROUGHNESSMAP
     
-        //         vec4 texelRoughness = texture2D( roughnessMap, vRoughnessMapUv );
-        //         // reversing the black and white values because we provide the ocean map
-        //         texelRoughness = vec4(1.0) - texelRoughness;
+                vec4 texelRoughness = texture2D( roughnessMap, vRoughnessMapUv );
+                // reversing the black and white values because we provide the ocean map
+                texelRoughness = vec4(1.0) - texelRoughness;
     
-        //         // reads channel G, compatible with a combined OcclusionRoughnessMetallic (RGB) texture
-        //         roughnessFactor *= clamp(texelRoughness.g, 0.5, 1.0);
+                // reads channel G, compatible with a combined OcclusionRoughnessMetallic (RGB) texture
+                roughnessFactor *= clamp(texelRoughness.g, 0.5, 1.0);
     
-        //     #endif
-        //     `);
-        //     shader.fragmentShader = shader.fragmentShader.replace('#include <emissivemap_fragment>', `
-        //     #ifdef USE_EMISSIVEMAP
+            #endif
+            `);
+            // shader.fragmentShader = shader.fragmentShader.replace('#include <emissivemap_fragment>', `
+            // #ifdef USE_EMISSIVEMAP
     
-        //         vec4 emissiveColor = texture2D( emissiveMap, vEmissiveMapUv );
+            //     vec4 emissiveColor = texture2D( emissiveMap, vEmissiveMapUv );
     
-        //         // Methodology of showing night lights only:
-        //         //
-        //         // going through the shader calculations in the meshphysical shader chunks (mostly on the vertex side),
-        //         // we can confirm that geometryNormal is the normalized normal in view space,
-        //         // for the night side of the earth, the dot product between geometryNormal and the directional light would be negative
-        //         // since the direction vector actually points from target to position of the DirectionalLight,
-        //         // for lit side of the earth, the reverse happens thus emissiveColor would be multiplied with 0.
-        //         // The smoothstep is to smoothen the change between night and day
+            //     // Methodology of showing night lights only:
+            //     //
+            //     // going through the shader calculations in the meshphysical shader chunks (mostly on the vertex side),
+            //     // we can confirm that geometryNormal is the normalized normal in view space,
+            //     // for the night side of the earth, the dot product between geometryNormal and the directional light would be negative
+            //     // since the direction vector actually points from target to position of the DirectionalLight,
+            //     // for lit side of the earth, the reverse happens thus emissiveColor would be multiplied with 0.
+            //     // The smoothstep is to smoothen the change between night and day
                 
-        //         emissiveColor *= 1.0 - smoothstep(-0.02, 0.0, dot(geometryNormal, directionalLights[0].direction));
+            //     emissiveColor *= 1.0 - smoothstep(-0.02, 0.0, dot(geometryNormal, directionalLights[0].direction));
                 
-        //         totalEmissiveRadiance *= emissiveColor.rgb;
+            //     totalEmissiveRadiance *= emissiveColor.rgb;
     
-        //     #endif
+            // #endif
     
-        //     // Methodology explanation:
-        //     //
-        //     // Our goal here is to use a “negative light map” approach to cast cloud shadows,
-        //     // the idea is on any uv point on earth map(Point X),
-        //     // we find the corresponding uv point(Point Y) on clouds map that is directly above Point X,
-        //     // then we extract color value at Point Y.
-        //     // We then darken the color value at Point X depending on the color value at Point Y,
-        //     // that is the intensity of the clouds at Point Y.
-        //     //
-        //     // Since the clouds are made to spin twice as fast as the earth,
-        //     // in order to get the correct shadows(clouds) position in this earth's fragment shader
-        //     // we need to minus earth's UV.x coordinate by uv_xOffset,
-        //     // which is calculated and explained in the updateScene()
-        //     // after minus by uv_xOffset, the result would be in the range of -1 to 1,
-        //     // we need to set RepeatWrapping for wrapS of the clouds texture so that texture2D still works for -1 to 0
+            // // Methodology explanation:
+            // //
+            // // Our goal here is to use a “negative light map” approach to cast cloud shadows,
+            // // the idea is on any uv point on earth map(Point X),
+            // // we find the corresponding uv point(Point Y) on clouds map that is directly above Point X,
+            // // then we extract color value at Point Y.
+            // // We then darken the color value at Point X depending on the color value at Point Y,
+            // // that is the intensity of the clouds at Point Y.
+            // //
+            // // Since the clouds are made to spin twice as fast as the earth,
+            // // in order to get the correct shadows(clouds) position in this earth's fragment shader
+            // // we need to minus earth's UV.x coordinate by uv_xOffset,
+            // // which is calculated and explained in the updateScene()
+            // // after minus by uv_xOffset, the result would be in the range of -1 to 1,
+            // // we need to set RepeatWrapping for wrapS of the clouds texture so that texture2D still works for -1 to 0
     
-        //     float cloudsMapValue = texture2D(tClouds, vec2(vMapUv.x - uv_xOffset, vMapUv.y)).r;
+            // float cloudsMapValue = texture2D(tClouds, vec2(vMapUv.x - uv_xOffset, vMapUv.y)).r;
             
-        //     // The shadow should be more intense where the clouds are more intense,
-        //     // thus we do 1.0 minus cloudsMapValue to obtain the shadowValue, which is multiplied to diffuseColor
-        //     // we also clamp the shadowValue to a minimum of 0.2 so it doesn't get too dark
+            // // The shadow should be more intense where the clouds are more intense,
+            // // thus we do 1.0 minus cloudsMapValue to obtain the shadowValue, which is multiplied to diffuseColor
+            // // we also clamp the shadowValue to a minimum of 0.2 so it doesn't get too dark
             
-        //     diffuseColor.rgb *= max(1.0 - cloudsMapValue, 0.2 );
+            // diffuseColor.rgb *= max(1.0 - cloudsMapValue, 0.2 );
     
-        //     // adding small amount of atmospheric coloring to make it more realistic
-        //     // fine tune the first constant for stronger or weaker effect
-        //     float intensity = 1.4 - dot( geometryNormal, vec3( 0.0, 0.0, 1.0 ) );
-        //     vec3 atmosphere = vec3( 0.3, 0.6, 1.0 ) * pow(intensity, 5.0);
-        //     diffuseColor.rgb += atmosphere;
-        //     `)
+            // // adding small amount of atmospheric coloring to make it more realistic
+            // // fine tune the first constant for stronger or weaker effect
+            // float intensity = 1.4 - dot( geometryNormal, vec3( 0.0, 0.0, 1.0 ) );
+            // vec3 atmosphere = vec3( 0.3, 0.6, 1.0 ) * pow(intensity, 5.0);
+            // diffuseColor.rgb += atmosphere;
+            // `)
     
-        //     // need save to userData.shader in order to enable our code to update values in the shader uniforms,
-        //     // reference from https://github.com/mrdoob/three.js/blob/master/examples/webgl_materials_modified.html
-        //    // earthMat.userData.shader = shader
-        // }
+            // need save to userData.shader in order to enable our code to update values in the shader uniforms,
+            // reference from https://github.com/mrdoob/three.js/blob/master/examples/webgl_materials_modified.html
+           // earthMat.userData.shader = shader
+        }
     return this
     };
     update (interval){
